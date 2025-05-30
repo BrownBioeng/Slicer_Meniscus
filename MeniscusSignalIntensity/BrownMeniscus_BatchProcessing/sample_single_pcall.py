b0872fe1ec533aaa343ad32332805ea2b3b8224c
amymmorton <65619862+amymmorton@users.noreply.github.com>
2025-05-30 16:08:53 -0400

    ENH: Batch meniscus files from folder

--- MeniscusSignalIntensity/BrownMeniscus_BatchProcessing/sample_single_pcall.py
+++ MeniscusSignalIntensity/BrownMeniscus_BatchProcessing/sample_single_pcall.py
@@ -1,5 +1,44 @@
-import glob
-import os
+import slicer
+import DICOMLib
+from DICOMLib import DICOMUtils
+from DICOM import DICOMWidget #slicer.util.selectModule("DICOM")
+from MeniscusSignalIntensity import MeniscusSignalIntensityLogic as mL
+
+
+dcm_folder = 'P:\\DBarnes\\Meniscus\\Slicer_6mo_data\\BEAR_II_100_6_M_left_Sx_BEAR_CISS\\B2_100_6mo'
+mm_stl = 'P:\\DBarnes\\Meniscus\\Slicer_6mo_data\\BEAR_II_100_6_M_left_Sx_BEAR_CISS\\BEAR_II_100_6_M_left_Sx_BEAR_CISS_MM.stl'
+lm_stl = 'P:\\DBarnes\\Meniscus\\Slicer_6mo_data\\BEAR_II_100_6_M_left_Sx_BEAR_CISS\\BEAR_II_100_6_M_left_Sx_BEAR_CISS_LM.stl'
+anatomy = 'left'
+
+outdir ='P:\\DBarnes\\Meniscus\\Slicer_6mo_data\\_csvSignalIntensity'
+
+db = slicer.dicomDatabase
+
+DICOMLib.clearDatabase(slicer.dicomDatabase)
+slicer.mrmlScene.Clear(0)
+
+dicomBrowser = slicer.modules.DICOMWidget.browserWidget.dicomBrowser
+dicomBrowser.importDirectory(dcm_folder, dicomBrowser.ImportDirectoryAddLink)
+  # wait for import to finish before proceeding (optional, if removed then import runs in the background)
+dicomBrowser.waitForImportFinished()
+
+
+browserWidget = slicer.modules.DICOMWidget.browserWidget
+
+patient = db.patients()[0]
+studies = db.studiesForPatient(patient)
+series = [db.seriesForStudy(study) for study in studies]
+seriesUIDs = [uid for uidList in series for uid in uidList]
+
+browserWidget.onSeriesSelected(seriesUIDs)
+browserWidget.loadCheckedLoadables()
+
+inputVolume = slicer.mrmlScene.GetFirstNodeByClass("vtkMRMLScalarVolumeNode")
+medModel = slicer.util.loadModel(mm_stl)
+latModel = slicer.util.loadModel(lm_stl)
+
+#mNode = mL.getParameterNode()
+mWidget = slicer.modules.meniscussignalintensity.widgetRepresentation().self()
 
 
 def meniscusSItoTable(inputVolume, medModel, latModel, anatomy):
@@ -119,67 +158,4 @@ def meniscusSItoTable(inputVolume, medModel, latModel, anatomy):
         resTable
     )
 
-
-
-
-# collect folder names 
-folder_names = glob.glob(r"P:\DBarnes\Meniscus\Slicer_6mo_data\*")
-
-# Remove any in the list that don't start with BEAR
-folders = []
-for i in folder_names:
-    if 'BEAR' in i:
-        folders.append(i)
-
-for files in folders:
-    folder = os.path.join(files,"*")
-    folder_open = glob.glob(folder)
-    #Assign STLs path variables
-    for item in folder_open:
-        if item.endswith('_MM.stl'):
-            mm_stl = item
-        elif item.endswith('_LM.stl'):
-            lm_stl = item
-    # Assign Dicom Folder 
-        for dirpath, dirnames, filenames in os.walk(item):
-            if any(filename.lower().endswith('.dcm') for filename in filenames):
-                dcm_folder = dirpath
-    #Right or left 
-    if "right" in files:
-        anatomy = "right"
-    else:
-        anatomy = "left"
-    
-
-        
-    outdir ='P:\\DBarnes\\Meniscus\\Slicer_6mo_data\\_csvSignalIntensity'
-
-    db = slicer.dicomDatabase
-
-    DICOMLib.clearDatabase(slicer.dicomDatabase)
-    slicer.mrmlScene.Clear(0)
-
-    dicomBrowser = slicer.modules.DICOMWidget.browserWidget.dicomBrowser
-    dicomBrowser.importDirectory(dcm_folder, dicomBrowser.ImportDirectoryAddLink)
-    # wait for import to finish before proceeding (optional, if removed then import runs in the background)
-    dicomBrowser.waitForImportFinished()
-
-
-    browserWidget = slicer.modules.DICOMWidget.browserWidget
-
-    patient = db.patients()[0]
-    studies = db.studiesForPatient(patient)
-    series = [db.seriesForStudy(study) for study in studies]
-    seriesUIDs = [uid for uidList in series for uid in uidList]
-
-    browserWidget.onSeriesSelected(seriesUIDs)
-    browserWidget.loadCheckedLoadables()
-
-    inputVolume = slicer.mrmlScene.GetFirstNodeByClass("vtkMRMLScalarVolumeNode")
-    medModel = slicer.util.loadModel(mm_stl)
-    latModel = slicer.util.loadModel(lm_stl)
-
-    #mNode = mL.getParameterNode()
-    mWidget = slicer.modules.meniscussignalintensity.widgetRepresentation().self()
-
-    meniscusSItoTable(inputVolume, medModel, latModel, anatomy)
+meniscusSItoTable(inputVolume, medModel, latModel, anatomy)
